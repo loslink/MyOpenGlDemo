f08c43500da70dbbbd9936e7137827c414f25375
loslink <965918077@qq.com>
2019-09-24 22:54:51 +0800

    new

--- app/src/main/java/com/loslink/myopengldemo/MainActivity.java
+++ app/src/main/java/com/loslink/myopengldemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.loslink.myopengldemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -7,16 +8,28 @@ import android.widget.TextView;
 import com.loslink.myopengldemo.renderer.NewRenderer;
 import com.loslink.myopengldemo.view.CircleGLSurfaceView;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    @OnClick(R.id.bt_filter_image)
+    public void bt_filter_image(){
+        Intent intent=new Intent(this,ImageFilterActivity.class);
+        startActivity(intent);
     }
+
+    @OnClick(R.id.bt_image)
+    public void bt_image(){
+        Intent intent=new Intent(this,ImageActivity.class);
+        startActivity(intent);
+    }
+
 }
